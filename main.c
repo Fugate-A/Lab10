7f5d2f4a8c15468b168349df1bca30d527581623
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-04-19 13:23:12 -0400

    main, struct, insert

--- main.c
+++ main.c
@@ -5,6 +5,17 @@
 
 //------------------------------------------------------------
 #include <stdio.h>
+#include <stdlib.h>
+//------------------------------------------------------------
+
+//
+
+//------------------------------------------------------------
+struct Trie
+{
+    int count;
+    struct Trie *children[26];
+};
 //------------------------------------------------------------
 
 //
@@ -29,7 +40,24 @@ void insert(struct Trie **ppTrie, char *word)
 //------------------------------------------------------------
 int numberOfOccurances(struct Trie *pTrie, char *word)
 {
-    //
+    if( pTrie == NULL )
+    {
+        return 0;
+    }
+
+    for( int i = 0; word[i] != NULL; i++ )
+    {
+        int temp = word[i] - 'a';
+
+        pTrie = pTrie -> children[temp];
+
+        if( pTrie == NULL )
+        {
+            return 0;
+        }
+    }
+
+    return pTrie -> count;
 }
 //------------------------------------------------------------
 
@@ -45,18 +73,42 @@ struct Trie *deallocateTrie(struct Trie *pTrie)
 //
 
 //------------------------------------------------------------
+
+
+
+//------------------------------------------------------------
+
+//
+
+//------------------------------------------------------------
 int main(void)
 {
-      //read the number of the words in the dictionary
-      // parse line  by line, and insert each word to the trie data structure
-      char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
-      for (int i=0;i<5;i++)
-      {
-          printf("\t%s : %d\n",pWords[i], numberOfOccurances(&trie, pWords[i]));
-     }
+    char *pWords[] = {"notaword", "ucf", "no", "note", "corg"};
+
+    struct Trie *trie = NULL;
+
+    //read the number of the words in the dictionary
+    int size = sizeof(pWords) / sizeof(char);
+
+    // parse line  by line, and insert each word to the trie data structure
+    for( int i = 0; i < size; i++ )
+    {
+        insert( &trie, pWords[i] );
+    }
+    //
+    
+    for( int i = 0 ; i < size; i++ )
+    {
+        printf("\t%s : %d\n",pWords[i], numberOfOccurances(&trie, pWords[i]));
+    }
+    
     trie = deallocateTree(&trie);
-    if (trie != NULL)
-       printf("There is an error in this program\n");
+    
+    if( trie != NULL )
+    {
+        printf("There is an error in this program\n");
+    }
+
     return 0;
 }
 //------------------------------------------------------------
