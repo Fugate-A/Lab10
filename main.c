299c226e6e76c7c5a373e89b301d855aef48f6bd
Andrew Fugate <andrewfugate1121@yahoo.com>
2023-04-20 19:40:40 -0400

    semi-working code
    output showing
    1
    1
    1
    2
    2
    
    when it should be
    1
    1
    1
    1
    1

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 //------------------------------------------------------------
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 //------------------------------------------------------------
 
 //
@@ -30,8 +31,34 @@ struct Trie *deallocateTrie(struct Trie *pTrie);
 
 //------------------------------------------------------------
 void insert(struct Trie **ppTrie, char *word)
-{
-    //
+{ 
+    struct Trie *temp = *ppTrie;
+
+    if( temp == NULL )
+    {
+        temp = calloc( 1, sizeof( struct Trie ) );
+        *ppTrie = temp;
+    }
+ 
+    for( int alpha = 0; word[alpha] != '\0'; alpha++ )
+    {
+        char spot = word[alpha];
+
+        if( spot >= 'a' && spot <= 'z' )
+        {
+            spot = spot - 'a';
+
+            if( temp -> children[alpha] == NULL )
+            {
+                temp -> children[alpha] = calloc( 1, sizeof( struct Trie ) );
+            }
+
+            temp = temp -> children[alpha];
+        }
+    }
+
+    temp -> count++;
+
 }
 //------------------------------------------------------------
 
@@ -45,18 +72,22 @@ int numberOfOccurances(struct Trie *pTrie, char *word)
         return 0;
     }
 
-    for( int i = 0; word[i] != NULL; i++ )
+    for( int alpha = 0; word[alpha] != '\0'; alpha++ )
     {
-        int temp = word[i] - 'a';
+        char spot = word[alpha];
 
-        pTrie = pTrie -> children[temp];
-
-        if( pTrie == NULL )
+        if( spot >= 'a' && spot <= 'z' )
         {
-            return 0;
+            spot = spot - 'a';
+
+            pTrie = pTrie -> children[alpha];
+
+            if( pTrie == NULL )
+            {
+                return 0;
+            }
         }
     }
-
     return pTrie -> count;
 }
 //------------------------------------------------------------
@@ -66,16 +97,20 @@ int numberOfOccurances(struct Trie *pTrie, char *word)
 //------------------------------------------------------------
 struct Trie *deallocateTrie(struct Trie *pTrie)
 {
-    //
+    if( pTrie != NULL )
+    {
+        for( int i = 0; i < 26; i++ )
+        {
+            pTrie -> children[i] = deallocateTrie( pTrie -> children[i] );
+        }
+
+        free(pTrie);
+    }
+
+    free(pTrie);
+
+    return NULL;
 }
-//------------------------------------------------------------
-
-//
-
-//------------------------------------------------------------
-
-
-
 //------------------------------------------------------------
 
 //
@@ -88,7 +123,8 @@ int main(void)
     struct Trie *trie = NULL;
 
     //read the number of the words in the dictionary
-    int size = sizeof(pWords) / sizeof(char);
+    //int size = sizeof(pWords) / sizeof(char);
+    int size = 5;
 
     // parse line  by line, and insert each word to the trie data structure
     for( int i = 0; i < size; i++ )
@@ -96,13 +132,13 @@ int main(void)
         insert( &trie, pWords[i] );
     }
     //
-    
+
     for( int i = 0 ; i < size; i++ )
     {
-        printf("\t%s : %d\n",pWords[i], numberOfOccurances(&trie, pWords[i]));
+        printf("\t%s : %d\n",pWords[i], numberOfOccurances(trie, pWords[i]));
     }
     
-    trie = deallocateTree(&trie);
+    trie = deallocateTrie(trie);
     
     if( trie != NULL )
     {
